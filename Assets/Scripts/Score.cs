a9a711da0c92423d78affe33853d3ede74234885
Kaielth Craftlay <carlos.marquez.g@outlook.com>
2015-08-15 09:02:34 -0600

    Basic Resources and script files

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public Text ScoreText;
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		ScoreText.text = "Score: " + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
